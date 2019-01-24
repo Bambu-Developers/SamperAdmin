9ad67ae76bb0709c3886b1d299fc86057f2e1f3b
Ana Castellanos Bambu <ana.castellanos@bambu-mobile.com>
2019-01-24 16:41:47 -0600

    Service for list users

--- src/app/modules/dashboard/pages/users/pages/list-user/list-user.component.ts
+++ src/app/modules/dashboard/pages/users/pages/list-user/list-user.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserModel } from '../../models/user.model';
 import { USERS_LANGUAGE } from '../../data/language';
+import { UsersService } from '../../services/users.service';
 
-const ELEMENT_DATA: UserModel[] = [
-  { name: 'Ernesto David Salazar Molina', rol: 2, dateCreated: '27/Feb/2018', lastConexion: '27/Feb/2018', status: 0, edit: true },
-];
 
 @Component({
   selector: 'app-list-user',
@@ -15,11 +13,18 @@ export class ListUserComponent implements OnInit {
 
   public language = USERS_LANGUAGE;
   public displayedColumns: string[] = ['name', 'rol', 'dateCreated', 'lastConexion', 'status', 'edit'];
-  public dataSource = ELEMENT_DATA;
+  public dataSource: any;
 
-  constructor() { }
+  public users;
+
+  constructor(
+    private usersService: UsersService
+  ) { }
 
   ngOnInit() {
+    this.getsUsers();
   }
+
+  public getsUsers() {  }
 
 }
--- src/app/modules/dashboard/pages/users/services/users.service.ts
+++ src/app/modules/dashboard/pages/users/services/users.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { AngularFireList, AngularFireDatabase } from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(
+    public db: AngularFireDatabase,
+  ) { }
+
+  public getUsers() {
+    this.db.database.ref('Users').on('value', snapshot => {
+      return snapshot.val();
+    });
+  }
 }
