0f1d7b4e4a142ce5b8b4724f15d9898080183965
Ana Castellanos Bambu <ana.castellanos@bambu-mobile.com>
2019-01-24 11:33:37 -0600

    Add language users

--- src/app/modules/dashboard/pages/users/pages/list-user/list-user.component.ts
+++ src/app/modules/dashboard/pages/users/pages/list-user/list-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserModel } from '../../models/user.model';
+import { USERS_LANGUAGE } from '../../data/language';
 
 const ELEMENT_DATA: UserModel[] = [
   { name: 'Ernesto David Salazar Molina', rol: 2, dateCreated: '27/Feb/2018', lastConexion: '27/Feb/2018', status: 0, edit: true },
@@ -14,6 +15,7 @@ export class ListUserComponent implements OnInit {
 
   public displayedColumns: string[] = ['name', 'rol', 'dateCreated', 'lastConexion', 'status', 'edit'];
   public dataSource = ELEMENT_DATA;
+  public language = USERS_LANGUAGE;
 
   constructor() { }
 
